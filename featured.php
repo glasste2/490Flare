b7a3baba862e7ddef5dc28d3ba0c839e31df4bcc
glasste2 <150213211+glasste2@users.noreply.github.com>
2023-11-07 22:12:32 -0600

    Add files via upload

---
+++ featured.php
@@ -0,0 +1,32 @@
+<?php 
+$pageTitle = "Airports";
+include ("header.php"); ?>
+<h2>Our Featured Airports</h2>
+<h3>Look forward to search functionality coming soon!</h3>
+<!-- MKE -->
+<a href="airportfeed.php?iatacode=mke">
+<div id="featuredLeft">
+  <h3>MKE</h3><h4>Milwaukee Mitchell International</h4>
+</div>
+</a>
+
+
+<!-- JFK -->
+<a href="airportfeed.php?iatacode=jfk">
+<div id="featuredLeft">
+  <h3>JFK</h3><h4>New York JFK International</h4>
+</div>
+
+<!-- HND -->
+<a href="airportfeed.php?iatacode=HND">
+<div id="featuredLeft">
+  <h3>HND</h3><h4>Tokyo Haneda International</h4>
+</div>
+
+<!-- CDG -->
+<a href="airportfeed.php?iatacode=CDG">
+<div id="featuredLeft">
+  <h3>CDG</h3><h4>Paris Charles de Gaulle Airport</h4>
+</div>
+
+<? include ("footer.php")?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+$pageTitle = "Home";
+include("header.php") ?>
+
+<h2 class="clearFloat">Your airport guide, powered by fellow travelers.</h2>
+<img src="assets/jbuwing.png" class="ignorePadding">
+<h5><i>
+  Who knows travel better than travelers? At Flare, we aim to connect those expert travelers,
+  whether that be for business or leisure, to any traveler who desires more assistance in their travels.</i>
+</h5>
+<h3>Throw your Travel Troubles Away With Flare</h3>
+<p>
+    Experienced travelers are always there to help you by providing live updates throughout your trip, from Madison to Manila! To view live updates, view our <a href="featured.php">airports</a> from the airports page. Once on a page, View live posts, or add your own, giving others an important head's up on their travels. Feel free to edit or delete your update at any time!
+</p>
+<h3>Airport Information</h3>
+<p>
+    Learn about an airport before you touch down for your connection! We provide an overview of the airport; its top-rated restaurants, bars, and cafes; and an interactive map that is simple to navigate.
+</p>
+<?php include("footer.php"); ?>
